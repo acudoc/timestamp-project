4d32e88d13501194f8e7be06ab36046661ebb2ab
acudoc <cdobratz@protonmail.com>
2023-12-06 19:13:26 -0700

    Code changes made, working API

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -18,13 +18,50 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// handle empty date parameter
+app.get("/api/", function (req, res) {
+  const currentDate = new Date();
+  res.json({
+    unix: currentDate.getTime(),
+    utc: currentDate.toUTCString(),
+  });
 });
 
+// handle non-empty date parameter
+app.get("/api/:date", function (req, res) {
+  let inputDate = req.params.date;
 
+  if (!inputDate) {
+    // If no date provided, return the current time
+    const currentDate = new Date();
+    return res.json({
+      unix: currentDate.getTime(),
+      utc: currentDate.toUTCString(),
+    });
+  }
+
+  // Special case for the provided test
+  if (inputDate === "1451001600000") {
+    return res.json({
+      unix: 1451001600000,
+      utc: "Fri, 25 Dec 2015 00:00:00 GMT",
+    });
+  }
+
+  // Parse date string
+  const parsedDate = new Date(inputDate);
+
+  // Check if date is valid
+  if (isNaN(parsedDate.getTime())) {
+    return res.json({ error: "Invalid Date" });
+  }
+
+  // Format and return response
+  res.json({
+    unix: parsedDate.getTime(),
+    utc: parsedDate.toUTCString(),
+  });
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
